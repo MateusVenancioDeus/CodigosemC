69ad239a70fc932c9440afb011d168005ef6bc53
MATEUS VITOR VENANCIO DE DEUS <162513571+MateusVenancioDeus@users.noreply.github.com>
2025-06-16 21:15:15 -0300

    Gerador.c

--- Gerador.c
+++ Gerador.c
@@ -3,8 +3,12 @@
 #include <string.h>
 #include <time.h>
 
-time_t converter_para_timestap(int dia, int mes, int ano, int hora, int min, int seg) 
-{
+time_t converter_para_timestamp(int dia, int mes, int ano, int hora, int min, int seg) {
+    // Validações conforme pedido
+    if (dia == 0 || mes == 0 || ano == 0) return -1;
+    if (hora == 0 || hora > 24) return -1;
+    if (min < 0 || min > 59 || seg < 0 || seg > 59) return -1;
+
     struct tm t;
     t.tm_year = ano - 1900;
     t.tm_mon = mes - 1;
@@ -13,104 +17,61 @@ time_t converter_para_timestap(int dia, int mes, int ano, int hora, int min, int
     t.tm_min = min;
     t.tm_sec = seg;
     t.tm_isdst = -1;
-    time_t timestamp = mktime(&t);
-    if (timestamp == -1) {
-        printf("Data inválida. Tente novamente.\n");
-        return -1;
-    } else {
-        return timestamp;
-    }
+
+    return mktime(&t);
 }
 
-time_t gerar_timestamp_aleatorio(struct tm * inicial, struct tm * final) {
-    time_t timestamp_inicial, timestamp_final;
-    timestamp_inicial = mktime(inicial);
-    if (timestamp_inicial == -1) {
-        printf("Data inválida.\n");
-        return -1;
-    }
-    timestamp_final = mktime(final);
-    if (timestamp_final == -1) {
-        printf("Data inválida.\n");
-        return -1;
-    }
-    time_t timestamp_aleatorio = timestamp_inicial + rand() % (timestamp_final - timestamp_inicial + 1);
-    return timestamp_aleatorio;
+time_t gerar_timestamp_aleatorio(time_t inicio, time_t fim) {
+    return inicio + rand() % (fim - inicio + 1);
 }
 
-void generateRandomValue(char *buffer, const char *dataType) {
-    if (strcmp(dataType, "int") == 0) {
+void gerar_valor(char *buffer, const char *tipo) {
+    if (strcmp(tipo, "CONJ_Z") == 0) {
         sprintf(buffer, "%d", rand() % 1000);
-    } else if (strcmp(dataType, "bool") == 0) {
-        sprintf(buffer, "%s", (rand() % 2 == 0) ? "true" : "false");
-    } else if (strcmp(dataType, "float") == 0) {
-        sprintf(buffer, "%.2f", (float)rand() / RAND_MAX * 100.0);
-    } else if (strcmp(dataType, "string") == 0) {
+    } else if (strcmp(tipo, "CONJ_Q") == 0) {
+        sprintf(buffer, "%.2f", ((float)rand() / RAND_MAX) * 100.0);
+    } else if (strcmp(tipo, "TEXTO") == 0) {
         int len = 1 + rand() % 16;
-        for (int i = 0; i < len; i++) {
-            buffer[i] = 'a' + (rand() % 26);
-        }
+        for (int i = 0; i < len; i++) buffer[i] = 'a' + (rand() % 26);
         buffer[len] = '\0';
+    } else if (strcmp(tipo, "BINARIO") == 0) {
+        sprintf(buffer, "%s", (rand() % 2 == 0) ? "true" : "false");
     } else {
-        strcpy(buffer, "UNKNOWN");
+        strcpy(buffer, "TIPO_INVALIDO");
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 6 || (argc - 4) % 2 != 0) {
-        fprintf(stderr, "Uso: %s <data_inicio_dd/mm/aaaa_hh:mm:ss> <data_fim_dd/mm/aaaa_hh:mm:ss> <nome_sensor1> <tipo_dado1> [nome_sensor2 tipo_dado2 ...] <arquivo_saida>\n", argv[0]);
-        return 1;
-    }
+    if (argc < 6 || (argc - 4) % 2 != 0) return 1;
 
-    int start_day, start_month, start_year, start_hour, start_min, start_sec;
-    int end_day, end_month, end_year, end_hour, end_min, end_sec;
+    int dia_ini, mes_ini, ano_ini, hora_ini, min_ini, seg_ini;
+    int dia_fim, mes_fim, ano_fim, hora_fim, min_fim, seg_fim;
 
-    if (sscanf(argv[1], "%d/%d/%d_%d:%d:%d", &start_day, &start_month, &start_year, &start_hour, &start_min, &start_sec) != 6) {
-        fprintf(stderr, "Formato de data/hora de início inválido. Use dd/mm/aaaa_hh:mm:ss\n");
-        return 1;
-    }
-    time_t start_timestamp = converter_para_timestap(start_day, start_month, start_year, start_hour, start_min, start_sec);
-    if (start_timestamp == -1) return 1;
+    if (sscanf(argv[1], "%d/%d/%d_%d:%d:%d", &dia_ini, &mes_ini, &ano_ini, &hora_ini, &min_ini, &seg_ini) != 6) return 1;
+    if (sscanf(argv[2], "%d/%d/%d_%d:%d:%d", &dia_fim, &mes_fim, &ano_fim, &hora_fim, &min_fim, &seg_fim) != 6) return 1;
 
-    if (sscanf(argv[2], "%d/%d/%d_%d:%d:%d", &end_day, &end_month, &end_year, &end_hour, &end_min, &end_sec) != 6) {
-        fprintf(stderr, "Formato de data/hora de fim inválido. Use dd/mm/aaaa_hh:mm:ss\n");
-        return 1;
-    }
-    time_t end_timestamp = converter_para_timestap(end_day, end_month, end_year, end_hour, end_min, end_sec);
-    if (end_timestamp == -1) return 1;
+    time_t timestamp_ini = converter_para_timestamp(dia_ini, mes_ini, ano_ini, hora_ini, min_ini, seg_ini);
+    time_t timestamp_fim = converter_para_timestamp(dia_fim, mes_fim, ano_fim, hora_fim, min_fim, seg_fim);
 
-    if (start_timestamp > end_timestamp) {
-        fprintf(stderr, "Intervalo de data/hora inválido: data de início é maior que a data de fim.\n");
-        return 1;
-    }
+    if (timestamp_ini == -1 || timestamp_fim == -1 || timestamp_ini > timestamp_fim) return 1;
 
-    char *outputFileName = argv[argc - 1];
-    FILE *outputFile = fopen(outputFileName, "w");
-    if (outputFile == NULL) {
-        perror("Erro ao criar arquivo de saída");
-        return 1;
-    }
+    FILE *saida = fopen(argv[argc - 1], "w");
+    if (saida == NULL) return 1;
 
     srand(time(NULL));
 
-    struct tm tm_start_gen = *localtime(&start_timestamp);
-    struct tm tm_end_gen = *localtime(&end_timestamp);
-
     for (int i = 3; i < argc - 1; i += 2) {
-        char *sensorName = argv[i];
-        char *dataType = argv[i+1];
+        char *sensor = argv[i];
+        char *tipo = argv[i + 1];
 
         for (int j = 0; j < 2000; j++) {
-            time_t randomTimestamp = gerar_timestamp_aleatorio(&tm_start_gen, &tm_end_gen);
-            char valueBuffer[256];
-            generateRandomValue(valueBuffer, dataType);
-            fprintf(outputFile, "%ld %s %s\n", randomTimestamp, sensorName, valueBuffer);
+            time_t ts = gerar_timestamp_aleatorio(timestamp_ini, timestamp_fim);
+            char valor[256];
+            gerar_valor(valor, tipo);
+            fprintf(saida, "%ld %s %s\n", ts, sensor, valor);
         }
     }
 
-    fclose(outputFile);
-    printf("Arquivo teste '%s' gerado com sucesso.\n", outputFileName);
-
+    fclose(saida);
     return 0;
 }
-
